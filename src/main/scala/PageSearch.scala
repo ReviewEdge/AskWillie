fe266010b919f0b70bc019e2b65c7c84f1af4a58
Logan <reviewedge56@gmail.com>
2024-04-23 22:25:28 -0400

    tf works, tfidf does not

--- src/main/scala/PageSearch.scala
+++ src/main/scala/PageSearch.scala
@@ -1,5 +1,6 @@
 import scala.math.log
-import scala.collection.parallel.CollectionConverters._
+import scala.collection.parallel.CollectionConverters.*
+import scala.collection.parallel.ParMap
 
 object PageSearch {
     /**
@@ -8,8 +9,7 @@ object PageSearch {
      * @return       a list of the number of times any of the terms appeared in each page in the same order as given
      */
     def count(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        pages.map(page => query.map(tgt => page.text.sliding(tgt.length).count(window => window == tgt).toDouble).sum)//countOccurrences(page.text, query))
-        // TODO: implement this method and remove this stub
+        pages.par.map(page => query.map(tgt => page.text.sliding(tgt.length).count(window => window == tgt).toDouble).sum).toList//countOccurrences(page.text, query))
     }
 
     /**
@@ -18,8 +18,14 @@ object PageSearch {
      * @return      a list of the term-frequency of the occurrences of those terms in each page in the same order given
      */
     def tf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        List() // TODO: implement this method and remove this stub
+        pages.par.map(_.text.length).zip(count(pages, query)).map(_/_).toList
     }
+
+
+
+//    @main def runner(): Unit =
+//        println(tfidf)
+//
 
     /**
      * @param pages a list of RankedWebPage objects to be searched
@@ -27,6 +33,23 @@ object PageSearch {
      * @return      a list of the TF-IDF score for each page in the same order given
      */
     def tfidf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        List() // TODO: implement this method and remove this stub
+
+        // list of IDF for each term
+        val iDFS: ParMap[String, Double] = query.par.map(term => term -> log(pages.length / (pages.par.count(_.text.contains(term))+1))).toMap
+
+
+        def singleTermSinglePageTf(p: RankedWebPage, term: String): Double =
+            tf(List(p), List(term)).head
+
+//        val test = iDFS(query.head)
+
+
+        val test2 = query.fold(0.0)((accumulator, currentTerm) => accumulator + (singleTermSinglePageTf(pages.head, currentTerm) * iDFS.getOrElse(currentTerm, 1.0)))
+
+
+
+//        pages.map(p => query.reduce((accumulator, currentTerm) => accumulator + (singleTermSinglePageTf(p, currentTerm) * iDFS(currentTerm))))
+
+        List()
     }
 }
