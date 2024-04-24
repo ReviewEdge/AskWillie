6d48ada0426fd1984c6dd95df5111379a1dead0c
bryce-behr <91856574+bryce-behr@users.noreply.github.com>
2024-04-23 20:43:00 -0400

    pagerank parallelized

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -7,8 +7,8 @@ object PageRank {
      * @return      A map of page.id to a weight of 1.0 for those same WebPage objects
      */
     def equal(pages: Map[String, WebPage]): Map[String, Double] = {
-      // TODO: remove this stub and implement this method
-      pages.keys.map(_ -> 1.0).toMap
+        // TODO: remove this stub and implement this method
+        pages.keys.map(_ -> 1.0).toMap
     }
 
     /**
@@ -16,12 +16,12 @@ object PageRank {
      * @return A map of page.id to a weight that is a simple count of the number of pages linking to that page
      */
     def indegree(pages: Map[String, WebPage]): Map[String, Double] = {
-      // TODO: remove this stub and implement this method
-      pages.keys.par.map(str => str -> pages.values.count(page => page.links.contains(str)).toDouble).seq.toMap
+        // TODO: remove this stub and implement this method
+        pages.keys.par.map(str => str -> pages.values.count(page => page.links.contains(str)).toDouble).seq.toMap
     }
 
     def pagerank(pages: Map[String, WebPage]): Map[String, Double] = {
-//        Map() // TODO: remove this stub and implement this method
+        // TODO: remove this stub and implement this method
         val random = new scala.util.Random
         val dampingFactor = 0.85
         val walks = 10000
@@ -32,21 +32,19 @@ object PageRank {
             if (stepsLeft <= 0) startId
             else {
                 val currentPage = pages(startId)
-                val nextId = if (random.nextDouble() < dampingFactor && currentPage.links.nonEmpty)
-                    currentPage.links(random.nextInt(currentPage.links.size))
-                else
-                    pages.keys.toSeq(random.nextInt(pages.size))
+                val nextId = 
+                    if (random.nextDouble() < dampingFactor && currentPage.links.nonEmpty) currentPage.links(random.nextInt(currentPage.links.size)) 
+                    else pages.keys.toSeq(random.nextInt(pages.size))
                 randomWalk(nextId, stepsLeft - 1)
             }
         }
 
-        val finalPageCounts = (1 to walks).map { _ =>
+        val finalPageCounts = (1 to walks).par.map({ _ =>
             val startPageId = pages.keys.toSeq(random.nextInt(pages.size))
             randomWalk(startPageId, steps)
-        }.groupBy(identity).view.mapValues(_.size).toMap
+        }).seq.groupBy(identity).view.mapValues(_.size).toMap.par
 
-        val totalWalks = finalPageCounts.values.sum
-//        println(finalPageCounts.view.mapValues(count => (count + 1).toDouble / (totalWalks + pages.size)).toMap)
-        finalPageCounts.view.mapValues(count => (count + 1).toDouble / (totalWalks + pages.size)).toMap
+        val totalWalks = finalPageCounts.values.par.sum
+        finalPageCounts.seq.view.mapValues(count => (count + 1).toDouble / (totalWalks + pages.size)).toMap
     }
 }
